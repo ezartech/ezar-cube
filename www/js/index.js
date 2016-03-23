10a4d19c86cb89f4abcb48629f0e8222b9aed70c
Wayne <na>
2016-03-23 10:44:01 -0500

    initial check in

---
+++ www/js/index.js
@@ -0,0 +1,50 @@
+var startTime	= Date.now();
+var container;
+var camera, scene, renderer, stats;
+var cube;
+
+init();
+animate();
+
+function init() {
+
+	camera = new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.y = 150;
+	camera.position.z = 350;
+	//camera.target.position.y = 150;
+
+	scene = new THREE.Scene();
+
+	cube = new THREE.Mesh( new THREE.CubeGeometry( 200, 200, 200 ), new THREE.MeshNormalMaterial() );
+	cube.position.y = 150;
+
+	scene.add( cube );
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+}
+
+function animate() {
+	render();
+	requestAnimationFrame( animate );
+}
+
+function render() {
+	cube.rotation.x += 0.02;
+	cube.rotation.y += 0.0225;
+	cube.rotation.z += 0.0175;
+    
+	/*
+    var dtime	= Date.now() - startTime;
+	cube.scale.x	= 1.0 + 0.3*Math.sin(dtime/300);
+	cube.scale.y	= 1.0 + 0.3*Math.sin(dtime/300);
+	cube.scale.z	= 1.0 + 0.3*Math.sin(dtime/300);
+    */
+    
+	renderer.render( scene, camera );
+}
+
